b4a340d0763a4cc6b9ac6a754d483d03ac61c941
Ehsasinator <94863590+Ehsasinator@users.noreply.github.com>
2026-09-11 16:20:42 +0100

    ui added

--- gameLogic.js
+++ gameLogic.js
@@ -15,39 +15,89 @@ function getComputerChoice(){
     return result;
 }
 
-let humanChoiceInput = prompt("Enter rock paper scissors:");
+
+
+
+let aiScore = 0;
+let humScore = 0;
+
+
+
 
 function playRound(compChoice,humanChoice){
     console.log("i chose "+humanChoice);
     console.log("AI chose "+compChoice);
     if (compChoice == "rock" && humanChoice == "scissors"){
         console.log("AI wins!!!!")
+        winner = "AI wins!!!!"
     }
     if (compChoice == "rock" && humanChoice == "paper"){
         console.log("Human wins!!!!")
+        winner = "Human wins!!!!"
     }
     if (compChoice == "rock" && humanChoice == "rock"){
         console.log("no one wins!!!!")
+        winner = "no one wins!!!!"
     }
     if (compChoice == "scissors" && humanChoice == "scissors"){
         console.log("no one wins!!!!")
+        winner = "no one wins!!!!"
     }
     if (compChoice == "scissors" && humanChoice == "paper"){
         console.log("AI wins!!!!")
+        winner = "AI wins!!!!"
     }
     if (compChoice == "scissors" && humanChoice == "rock"){
         console.log("Human wins!!!!")
+        winner = "Human wins!!!!"
     }
     if (compChoice == "paper" && humanChoice == "scissors"){
         console.log("Human wins!!!!")
+        winner = "Human wins!!!!"
     }
     if (compChoice == "paper" && humanChoice == "paper"){
         console.log("no one wins!!!!")
+        winner = "no one wins!!!!"
     }
     if (compChoice == "paper" && humanChoice == "rock"){
         console.log("AI wins!!!!")
+        winner = "AI wins!!!!"
     }
+
+    if (winner == "AI wins!!!!") {
+        aiScore += 1;
+    }
+    if (winner == "Human wins!!!!") {
+        humScore += 1;
+    }
+    
+    
+    const score = document.querySelector("#score")
+
+    score.textContent = "SCORE = "+aiScore+"-"+humScore ;
+    const result = document.querySelector("#result")
+    if (aiScore > 4 && humScore < 5){
+        
+
+        result.textContent = "Winner is: AI";
+    }
+    if (humScore > 4 && aiScore < 5){
+        
+
+        result.textContent = "Winner is: Human";
+    }
+        
+        
 }
 
 
-playRound(getComputerChoice(),humanChoiceInput);
+let humanChoiceInput = "";
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+    button.addEventListener("click", (event) => {
+        humanChoiceInput = event.target.id;
+        playRound(getComputerChoice(),humanChoiceInput)
+    })
+})
